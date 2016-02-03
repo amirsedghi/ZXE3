d3da2d09176b66cbbb89b37e3ce1726f52761a5f
ArmandAbrahamian <armand.abrahamian@gmail.com>
2016-02-03 15:27:09 -0800

    I just wanted my shitty Dell to be included in this

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class main{ 
 	public static void main(String[] args) {
-		System.out.println("Hi guys!");
+		System.out.println("Hi guys! I am Armand and I love how shity my Dell laptop is. But seriously I love this guy!");
 	}
 }
