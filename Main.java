a9e03cdedb8bf3c97beb199bdfa1ee8ac3bd02fb
Jose Perez <joseluisp92@gmail.com>
2016-02-03 15:53:39 -0800

    pc

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class main{ 
 	public static void main(String[] args) {
-		System.out.println("Hi guys! I am Armand and I love how shity my Dell laptop is. But seriously I love this guy! *** fuck it all!!! mmmmmmmmmmmmm”);
+		System.out.println("Hi guys! I am Armand and I love how shity my Dell laptop is. But seriously I love this guy! *** fuck it all!!! mmmmmmmmmmmmm/////”);
 	}
 }
