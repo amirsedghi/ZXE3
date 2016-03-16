b02a6e2b066faa15449c80af545f791d0f42f2cd
Amir Sedghi <amir.sepultura@gmail.com>
2016-03-16 15:37:26 -0700

    final android stuff

--- android/testTwo/android/src/com/mygdx/game/AndroidLauncher.java
+++ android/testTwo/android/src/com/mygdx/game/AndroidLauncher.java
@@ -2,9 +2,9 @@ package com.mygdx.game;
 
 import android.os.Bundle;
 
+import com.badlogic.dd.DD;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
@@ -13,6 +13,6 @@ public class AndroidLauncher extends AndroidApplication {
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useAccelerometer = false;
 		config.useCompass = false;
-		initialize(new MyGdxGame(), config);
+		initialize(new DD(), config);
 	}
 }
