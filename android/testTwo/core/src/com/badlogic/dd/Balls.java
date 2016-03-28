6a9b8df43e526598de32fb021fa9d991a3d28272
Jose Perez <joseluisp92@gmail.com>
2016-03-28 16:01:07 -0700

    balls commit

---
+++ android/testTwo/core/src/com/badlogic/dd/Balls.java
@@ -0,0 +1,13 @@
+package com.badlogic.dd;
+
+/**
+ * Created by JosePerez on 3/28/16.
+ */
+public class Balls {
+    Balls(){
+
+    }
+    public static void printBalls(){
+        System.out.print("look at my balls...");
+    }
+}
