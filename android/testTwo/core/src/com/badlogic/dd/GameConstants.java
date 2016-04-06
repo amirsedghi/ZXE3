84a4a36a6a0a21886153032014fc9c61e2400911
GhostSonic <ghostsonic21@gmail.com>
2016-04-06 01:49:57 -0700

    Fix enemys not spawning if screen size changes

--- android/testTwo/core/src/com/badlogic/dd/GameConstants.java
+++ android/testTwo/core/src/com/badlogic/dd/GameConstants.java
@@ -15,6 +15,8 @@ public class GameConstants
     public static final float Y_MOVE_UNITS = 0.1f; // units will move in y direction
     public static final float maxVelocity = 0.1f;
     public static final float SKELETON_VELOCITY = 0.2f;
-    public static float screenWidth = Gdx.graphics.getWidth();
-    public static float screenHeight = Gdx.graphics.getHeight();
+    //public static float screenWidth = Gdx.graphics.getWidth();
+    //public static float screenHeight = Gdx.graphics.getHeight();
+    public static float screenWidth = 800;
+    public static float screenHeight = 480;
 }
